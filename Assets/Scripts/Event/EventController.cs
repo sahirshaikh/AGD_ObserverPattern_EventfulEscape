27666611a3b4f7748ee5ad93f065ce230a797125
Sahir Shaikh <shaikh.sahir50@gmail.com>
2024-06-07 17:06:55 +0530

    Create an Event Controller
    [TASKS]

---
+++ Assets/Scripts/Event/EventController.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System;
+
+public class EventController 
+{
+    public Action baseEvent;
+
+    public void AddListner(Action listner) => baseEvent += listner;
+    public void RemoveListner(Action listner) => baseEvent -= listner;
+
+    public void InvokeEvent() => baseEvent.Invoke();    
+
+
+}
